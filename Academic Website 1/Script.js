9da453a50963005d0e94b18e26ddaab1e51eacb8
ChillinRage <73763719+ChillinRage@users.noreply.github.com>
2023-01-05 11:42:43 +0800

    Update Script.js

--- Academic Website 1/Script.js
+++ Academic Website 1/Script.js
@@ -1,24 +1,26 @@
-function is_number(word) {
-    var regex = /^[a-zA-Z]+$/;
-    if (word.match(regex)) {
-        alert(word + ' is not a valid number.');
-        return false;
-    } else {
-        return true;
-    }
+function insert_row(data) {
+    var table  = document.getElementById("table");
+    var row    = table.insertRow();
+    var year   = row.insertCell(0);
+    var sem    = row.insertCell(1);
+    var mod    = row.insertCell(2);
+    var grade  = row.insertCell(3);
+    var mc     = row.insertCell(4);
+    var remark = row.insertCell(5);
+
+    year.innerHTML   = data[0];
+    sem.innerHTML    = data[1];
+    mod.innerHTML    = data[2];
+    grade.innerHTML  = data[3];
+    mc.innerHTML     = data[4];
+    remark.innerHTML = data[5];
 }
 
-function is_grade(word) {
-    if (word.length == 1) {
-        if (word.match(/^[A-D]$/) || word === 'F') {
-            return true;
-        }
-    } else if (word.length == 2) {
-        if (word.match(/^[A-B][+-]$/) || word.match(/^[C-D][+]$/)) {
-            return true;
-        }
-    }
-
-    alert('Invalid grade. Make sure that it is capitalised.');
-    return false;
+async function display() {
+    const url = 'https://github.com/ChillinRage/Basic-Projects/blob/main/Academic%20Website%201/NUS%20Acad%20results.csv';
+    const response = await fetch(url);       // fetch data
+    const raw_data = await response.text();  // process data
+    console.log(raw_data);
 }
+
+
